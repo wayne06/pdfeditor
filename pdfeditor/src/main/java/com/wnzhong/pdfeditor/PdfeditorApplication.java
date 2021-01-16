edde18c2fc2eeebf17c823512a35b9c4b6900267
wayne <zs577215@gmail.com>
2021-01-16 17:41:14 +0800

    abstract

--- pdfeditor/src/main/java/com/wnzhong/pdfeditor/PdfeditorApplication.java
+++ pdfeditor/src/main/java/com/wnzhong/pdfeditor/PdfeditorApplication.java
@@ -6,10 +6,13 @@ import com.itextpdf.text.pdf.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 @SpringBootApplication
 @RestController
@@ -19,10 +22,38 @@ public class PdfeditorApplication {
         SpringApplication.run(PdfeditorApplication.class, args);
     }
 
+    @PostMapping("/api/upload")
+    @CrossOrigin
+    public String coverUpload(MultipartFile file) {
+        File imgFolder = new File("F:\\pdf");
+        File f = new File(imgFolder, getRandomString(6) + file.getOriginalFilename().substring(file.getOriginalFilename().length() - 4));
+        if (!f.getParentFile().exists()) {
+            f.getParentFile().mkdirs();
+        }
+        try {
+            file.transferTo(f);
+            String filePath = f.getAbsolutePath();
+            return filePath;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    private String getRandomString(int length) {
+        String base = "abcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(base.length());
+            stringBuffer.append(base.charAt(number));
+        }
+        return stringBuffer.toString();
+    }
 
     @PostMapping("/api/extract")
     @CrossOrigin
-    public void extract(@RequestBody ExtractBody body) {
+    public String extract(@RequestBody ExtractBody body) {
         String path = body.getPath().replaceAll("\\\\", "\\\\\\\\");
         int from = body.getFrom();
         int to = body.getTo();
@@ -36,6 +67,7 @@ public class PdfeditorApplication {
             }
             ArrayList<String> savepaths = new ArrayList<>();
             String savepath = path.split("\\.")[0] + "_extract.pdf";
+            System.out.println(savepath);
             savepaths.add(savepath);
             document = new Document(reader.getPageSize(1));
             copy = new PdfCopy(document, new FileOutputStream(savepaths.get(0)));
@@ -46,12 +78,14 @@ public class PdfeditorApplication {
                 copy.addPage(page);
             }
             document.close();
-
+            String[] strings = savepath.split("\\\\");
+            return "http://localhost:8443/api/file/" + strings[strings.length-1];
         } catch (IOException e) {
             e.printStackTrace();
         } catch(DocumentException e) {
             e.printStackTrace();
         }
+        return "";
     }
 
     @PostMapping("/api/merge")
--- pdfeditor/src/main/java/com/wnzhong/pdfeditor/WebConfig.java
+++ pdfeditor/src/main/java/com/wnzhong/pdfeditor/WebConfig.java
@@ -2,6 +2,7 @@ package com.wnzhong.pdfeditor;
 
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootConfiguration
@@ -14,5 +15,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:8080")
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
                 .allowedHeaders("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/api/file/**")
+                .addResourceLocations("file:f:\\pdf\\");
     }
 }
